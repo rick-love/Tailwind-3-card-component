508c887b399fd1a82176d51f194ff9ac769bbabf
rick-love <ricklove@outlook.com>
2021-06-28 23:58:16 +0200

    BUsed Tailwind to build 3card component

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brightOrange: '#e38826',
+        cyan: '#006970',
+        darkCyan: '#004241',
+        paraWhite: '#ffffff',
+        lightGray: '#f2f2f2'
+      },
+      fontFamily: {
+        body: ['Lexend Deca'],
+        heading: ['Big Shoulders Display']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
